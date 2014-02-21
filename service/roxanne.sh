2a40305e95d4d77f59b4e07f2eb90c0e362bec08
Jef Mathiot <jeff.mathiot@gmail.com>
2014-02-21 12:52:03 +0100

    Fixed startup script and replace RVM with rbenv.

--- service/roxanne.sh
+++ service/roxanne.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $HOME/roxanne
-source $HOME/.bashrc
-rvm 1.9.2@roxanne
-ruby lib/roxanne_server.rb $1 config/golgotha.yml
+source $HOME/.bash_profile
+eval "$(rbenv init -)"
+cd /var/lib/roxanne/roxanne
+bundle exec ruby lib/roxanne_server.rb $1 config/golgotha.yml
